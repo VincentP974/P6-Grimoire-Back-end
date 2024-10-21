d8de320d091cce6d451526a953c6ea17a5ad3ce2
Vincent Payet <vincentp9@outlook.fr>
2024-10-21 11:43:49 +0400

    installation mongoose et configuration mongodb

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env.local" });
+const path = require("path");
 
-// Importing routes
-const bookRoutes = require('./routes/book');
-const userRoutes = require('./routes/user');
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-
+// Créer une application Express
 const app = express();
 
-//CORS
+//connect to database mongoDB
+mongoose.connect(`mongodb+srv://vincentp9:<vincentp9>@cluster0.k8frc.mongodb.net/`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+// Traitement des requêtes et réponses
+// Rendre les données exploitables en JSON
+app.use(express.json());
+
+// Éviter les problèmes de CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
     next();
 });
-
-app.use(express.json());
 
 // Routes
 app.use("/api/books", bookRoutes);
